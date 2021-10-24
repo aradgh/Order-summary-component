8627a4736ddedb225b1735db0f0efcdc7d5447f9
Arad GHORRESHI <arad.ghorreshi@gmail.com>
2021-10-25 00:35:08 +0200

    Adding centered div + Image + Title + paragraph

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
-
+import illustration_hero from "./images/illustration-hero.svg";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img src={illustration_hero} alt="illustration hero" />
+      <h1>Order summary</h1>
+      <p>
+        You can now listen to millions of songs, audiobooks, and podcasts on any
+        device anywhere you like!
+      </p>
+    </div>
+  );
 }
 
 export default App;
